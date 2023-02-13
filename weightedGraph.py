d68ac1839d15a6a85ba9bed4e6b29006f791dca7
Graziele-Rodrigues <c.graziele.rodrigues@gmail.com>
2023-02-13 15:07:56 -0300

    bellman_ford

--- weightedGraph.py
+++ weightedGraph.py
@@ -50,8 +50,8 @@ class WeightedGraph:
                 #for i in range(len(self.adj_list)):
                     #v = self.adj_list[u][i][0] #pega o vizinho
                     #w  = self.adj_list[u][i][1] #pega o peso
-                    if dist[v] > dist[u]+w(u,v):
-                        dist[v] = dist[u]+w(u,v)
+                    if dist[v] > dist[u]+w:
+                        dist[v] = dist[u]+w
                         pred[v] = u
 
 
